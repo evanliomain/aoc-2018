78db169d80818d28ce1ce76fad8b7f17b24c218d
Evan Liomain <evan.liomain@gmail.com>
2018-12-05 07:59:59 +0100

    05 1

--- 05/1.js
+++ 05/1.js
@@ -1,5 +1,22 @@
-const T = require('taninsam');
+const reaction = require('./reaction');
+
+// input
+// { c: 'a', n: 1 }[]
 
 module.exports = function(input) {
-  return 'TODO';
+  let i = 0;
+  let end = input.length;
+
+  while (0 !== end && 1 !== end && i < end - 1) {
+    if (0 !== reaction(input[i], input[i + 1], i)) {
+      i++;
+      continue;
+    }
+
+    input.splice(i, 2);
+    i = Math.max(0, i - 1);
+    end = input.length;
+  }
+
+  return end;
 };
