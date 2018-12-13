97c6eebedc135aaef06ede8d1d0923de69df2940
Evan Liomain <evan.liomain@gmail.com>
2018-12-13 12:31:32 +0100

    Day 13 1

--- 13/in.js
+++ 13/in.js
@@ -1,5 +1,25 @@
 const T = require('taninsam');
 
 module.exports = function(input) {
-  return input;
+  const carts = [];
+  return T.chain(input)
+    .chain(T.map(T.split()))
+    .chain(
+      T.map((row, y) =>
+        row.map((cell, x) => {
+          if ('>' === cell || '<' === cell || '^' === cell || 'v' === cell) {
+            carts.push({ direction: cell, x, y, counter: 0 });
+          }
+          if ('>' === cell || '<' === cell) {
+            return '-';
+          }
+          if ('^' === cell || 'v' === cell) {
+            return '|';
+          }
+          return cell;
+        })
+      )
+    )
+    .chain(circuit => ({ circuit, carts }))
+    .value();
 };
