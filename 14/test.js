d90271da56f7d02904bc3a09b89657836830b3cd
Evan Liomain <evan.liomain@gmail.com>
2018-12-14 20:26:24 +0100

    Cleanup day 14 test

--- 14/test.js
+++ 14/test.js
@@ -2,8 +2,6 @@ const solve1 = require('./1');
 const solve2 = require('./2');
 const parse = require('./in');
 const get = require('../input');
-const input = parse(get('14'));
-const sample = parse(get('14', true));
 
 describe('14', () => {
   it('solve 1', () => {
@@ -11,7 +9,6 @@ describe('14', () => {
     expect(solve1(5)).toEqual('0124515891');
     expect(solve1(18)).toEqual('9251071085');
     expect(solve1(2018)).toEqual('5941429882');
-    // expect(solve1(input)).toMatchInlineSnapshot();
   });
 
   it('solve 2', () => {
