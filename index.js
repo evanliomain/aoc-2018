fa3240e55d543b03b765a597342e421139b22738
Evan Liomain <evan.liomain@gmail.com>
2018-12-15 12:24:19 +0100

    Upgrade exercice launcher

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
+const T = require('taninsam');
 const get = require('./input');
 const chalk = require('chalk');
 
 const [, , , day, exercice, sample] = process.argv;
 
-const isSample = undefined !== sample;
-
-const f = require(`./${day}/${exercice}`);
-
-let pre;
-try {
-  pre = require(`./${day}/in`);
-} catch (e) {
-  pre = x => x;
+let isSample = undefined !== sample;
+let isSampleNumber = false;
+if (isSample && !isNaN(parseInt(sample, 10))) {
+  isSample = parseInt(sample, 10);
+  isSampleNumber = true;
 }
 
+// Get solve function
+const solve = require(`./${day}/${exercice}`);
+
+// Get input parser
+let parser;
+try {
+  parser = require(`./${day}/in`);
+} catch (e) {
+  parser = x => x;
+}
+
+// Log user arguments
 console.log(
   [
     chalk.green(`day:`, chalk.bold(day)),
     chalk.blue(`exercice:`, chalk.bold(exercice)),
-    isSample ? chalk.red('sample data') : ''
+    (isSample ? chalk.red('sample data') : '') +
+      (isSampleNumber ? ' ' + chalk.bold.red(isSample) : '')
   ]
     .filter(x => '' !== x)
     .join(' - ')
 );
+
+// Get the input
+let input;
+try {
+  input = get(day, isSample);
+} catch (e) {
+  console.log(chalk.bold.red(e.message));
+  return;
+}
+
 console.time('solving in');
-const result = f(pre(get(day, isSample)));
+const result = solve(parser(input));
 console.timeEnd('solving in');
 
 if (typeof result === 'object') {
   console.log(
-    chalk.magenta('result:'),
-    chalk.bold(JSON.stringify(result, (_, v) => v, 2))
+    chalk.magenta('result:') + '\n' + JSON.stringify(result, (_, v) => v, 2)
   );
 } else {
-  console.log(chalk.magenta('result:'), chalk.bold(result));
+  console.log(chalk.magenta('result:') + '\n' + result);
 }
--- input.js
+++ input.js
@@ -1,8 +1,17 @@
+const T = require('taninsam');
 const fs = require('fs');
 
 module.exports = function get(n, isSample) {
+  let suffix = '';
+  if (isSample) {
+    suffix = '.sample';
+  }
+  if (T.isNumber(isSample)) {
+    suffix += isSample;
+  }
+
   return fs
-    .readFileSync('./input/' + n + (isSample ? '.sample' : ''), 'utf8')
+    .readFileSync('./input/' + n + suffix, 'utf8')
     .split('\n')
     .filter(x => '' !== x);
 };
